287e0ac721582bf05b80deed516a212b04e1f8b6
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-12-26 16:26:00 -0800

    added to greyscale output test

--- tests/main_tests.rs
+++ tests/main_tests.rs
@@ -1,5 +1,7 @@
 use image::io::Reader as ImageReader;
 
+const TEST_OUTPUT_DIR: &str = "./tests/output/";
+
 #[test]
 fn test_load_image() {
     let _image = ImageReader::open("./test_images/window_only.png").unwrap().decode().unwrap();
@@ -8,5 +10,13 @@ fn test_load_image() {
 #[test]
 fn test_save_image() {
     let image = ImageReader::open("./test_images/window_only.png").unwrap().decode().unwrap();
-    image.save("./tests/output/saved_image_test_file.png").unwrap();
+    image.save(TEST_OUTPUT_DIR.to_owned() + "saved_image_test_file.png").unwrap();
+}
+
+
+#[test]
+fn test_grayscale() {
+    let image = ImageReader::open("./test_images/window_only.png").unwrap().decode().unwrap();
+    image.to_luma8();
+    image.save(TEST_OUTPUT_DIR.to_owned() + "greyscale_window.png").unwrap();
 }
