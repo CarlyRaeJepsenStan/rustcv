056dc9d7a822df5321aa4fd7ab200b9b3adf10ec
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-12-26 16:21:47 -0800

    added image save test

--- tests/main_tests.rs
+++ tests/main_tests.rs
@@ -4,3 +4,9 @@ use image::io::Reader as ImageReader;
 fn test_load_image() {
     let _image = ImageReader::open("./test_images/window_only.png").unwrap().decode().unwrap();
 }
+
+#[test]
+fn test_save_image() {
+    let image = ImageReader::open("./test_images/window_only.png").unwrap().decode().unwrap();
+    image.save("./tests/output/saved_image_test_file.png").unwrap();
+}
