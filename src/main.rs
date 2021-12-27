523019ad7e9421ee15e75bf4a6e2fa3e121ad2b5
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-12-26 17:57:05 -0800

    renamed to be more accurate

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,6 @@ fn main() {
 
 fn create_first_derivative_nick_1() {
     let image = ImageReader::open("./test_images/window_only.png").unwrap().decode().unwrap();
-    let first_div = first_derivative::calc_first_derivative_image(&image);
+    let first_div = first_derivative::calc_first_derivative_image_horizontal(&image);
     first_div.save("./output/first_div_horizontal.png").unwrap();
 }
