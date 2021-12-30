cb7576b7d91322212c4de53dfdf5a48d4ff52422
CarlyRaeJepsenStan <44917034+CarlyRaeJepsenStan@users.noreply.github.com>
2021-12-29 17:23:56 -0800

    Added function to pad matrices with 0s

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,16 @@
 use image::{DynamicImage, ImageBuffer, Luma};
+use nalgebra::DMatrix;
 
 
 pub fn to_greyscale(input: &DynamicImage) -> ImageBuffer<Luma<u8>, Vec<u8>> {
     let greyscale_input = input.to_luma8();
 
     greyscale_input
+}
+
+pub fn pad(mt: DMatrix<f32>) -> DMatrix<f32> {
+    let r = mt.nrows();
+    let c = mt.ncols();
+    let padded = mt.insert_row(r,0.0).insert_row(0,0.0).insert_columns(c,1,0.0).insert_columns(0,1,0.0);
+    return padded;
 }
